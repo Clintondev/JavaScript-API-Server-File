341f8b3c5bb498fa0579ddd079b06139324b8cbc
Clintondev <clintonrodrigues.dev@gmail.com>
2023-09-04 13:46:03 -0300

    update

--- public/script.js
+++ public/script.js
@@ -1,42 +1,86 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fileList = document.getElementById('fileList');
-    const fileInput = document.getElementById('fileInput');
+    const modal = document.getElementById('modal');
+    const confirmButton = document.getElementById('confirm-button');
+    const cancelButton = document.getElementById('cancel-button');
+    let deletingFile = ''; // Para armazenar o nome do arquivo a ser excluído
   
     // Função para listar os arquivos disponíveis para download
     const listFiles = async () => {
       fileList.innerHTML = '';
-      const response = await fetch('/list-files');
-      const files = await response.json();
+      try {
+        const response = await fetch('/list-files');
+        const files = await response.json();
   
-      files.forEach((file) => {
-        const listItem = document.createElement('li');
-        const downloadLink = document.createElement('a');
-        downloadLink.href = `/download/${file}`;
-        downloadLink.textContent = file;
-        listItem.appendChild(downloadLink);
-        fileList.appendChild(listItem);
-      });
+        files.forEach((file) => {
+          const listItem = document.createElement('li');
+          const downloadLink = document.createElement('a');
+          const deleteButton = document.createElement('button');
+  
+          downloadLink.href = `/download/${file}`;
+          downloadLink.textContent = file;
+          deleteButton.textContent = 'Excluir';
+          deleteButton.classList.add('delete-button');
+          deleteButton.setAttribute('data-filename', file); // Adicionar o nome do arquivo como atributo de dados
+  
+          listItem.appendChild(downloadLink);
+          listItem.appendChild(deleteButton);
+          fileList.appendChild(listItem);
+  
+          // Evento para abrir o modal quando o botão Excluir é clicado
+          deleteButton.addEventListener('click', () => {
+            const filename = deleteButton.getAttribute('data-filename');
+            openModal(filename);
+          });
+        });
+      } catch (error) {
+        console.error('Erro ao listar os arquivos:', error);
+      }
     };
+  
+    // Função para abrir o modal de confirmação
+    const openModal = (filename) => {
+      deletingFile = filename;
+      modal.style.display = 'block';
+    };
+  
+    // Função para fechar o modal de confirmação
+    const closeModal = () => {
+      modal.style.display = 'none';
+      deletingFile = '';
+    };
+  
+    // Função para excluir o arquivo
+    const deleteFile = async (filename) => {
+      try {
+        const response = await fetch(`/delete/${filename}`, {
+          method: 'DELETE',
+        });
+  
+        if (response.ok) {
+          closeModal();
+          listFiles(); // Atualize a lista de arquivos após a exclusão bem-sucedida
+        } else {
+          console.error('Erro ao excluir o arquivo:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Erro ao excluir o arquivo:', error);
+      }
+    };
+  
+    // Evento para fechar o modal quando o botão Cancelar é clicado
+    cancelButton.addEventListener('click', () => {
+      closeModal();
+    });
+  
+    // Evento para excluir o arquivo quando o botão Confirmar é clicado
+    confirmButton.addEventListener('click', async () => {
+      if (deletingFile) {
+        deleteFile(deletingFile);
+      }
+    });
   
     // Atualize a lista de arquivos quando a página for carregada
     listFiles();
-  
-    // Atualize a lista de arquivos após um novo arquivo ter sido enviado
-    document.querySelector('form').addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const formData = new FormData();
-      formData.append('file', fileInput.files[0]);
-  
-      try {
-        await fetch('/upload', {
-          method: 'POST',
-          body: formData,
-        });
-        fileInput.value = ''; // Limpar o campo de seleção de arquivo
-        listFiles(); // Atualizar a lista de arquivos
-      } catch (error) {
-        console.error('Erro ao enviar o arquivo:', error);
-      }
-    });
   });
   
--- server.js
+++ server.js
@@ -23,8 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define uma rota para upload de arquivos
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.send('Arquivo enviado com sucesso');
+  res.json({ message: 'Arquivo enviado com sucesso' });
 });
+
 
 // Define uma rota para listar os arquivos disponíveis para download
 app.get('/list-files', (req, res) => {
@@ -46,6 +47,19 @@ app.get('/download/:filename', (req, res) => {
 
   if (fs.existsSync(filePath)) {
     res.download(filePath);
+  } else {
+    res.status(404).send('Arquivo não encontrado');
+  }
+});
+
+// Define uma rota para excluir arquivos
+app.delete('/delete/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename);
+
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+    res.send('Arquivo excluído com sucesso');
   } else {
     res.status(404).send('Arquivo não encontrado');
   }
