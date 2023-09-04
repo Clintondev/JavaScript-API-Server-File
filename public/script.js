ede6ba72c6970ccea6d4264008022c440aabffa1
Clintondev <clintonrodrigues.dev@gmail.com>
2023-09-04 14:37:14 -0300

    update

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('modal');
     const confirmButton = document.getElementById('confirm-button');
     const cancelButton = document.getElementById('cancel-button');
+    const fileInput = document.getElementById('fileInput'); // Adicionado para referenciar o campo de seleção de arquivo
     let deletingFile = ''; // Para armazenar o nome do arquivo a ser excluído
   
     // Função para listar os arquivos disponíveis para download
@@ -79,6 +80,54 @@ document.addEventListener('DOMContentLoaded', () => {
         deleteFile(deletingFile);
       }
     });
+  
+    // Intercepte o envio do formulário e envie o arquivo via AJAX
+    const uploadForm = document.querySelector('.upload-form');
+  
+    uploadForm.addEventListener('submit', async (e) => {
+      e.preventDefault(); // Evita o envio tradicional do formulário
+  
+      const formData = new FormData();
+      formData.append('file', fileInput.files[0]);
+  
+      try {
+        const response = await fetch('/upload', {
+          method: 'POST',
+          body: formData,
+        });
+  
+        if (response.ok) {
+          // Limpe o campo de seleção de arquivo após o envio bem-sucedido
+          fileInput.value = '';
+          
+          // Atualize a lista de arquivos após o envio bem-sucedido
+          listFiles();
+          showMessage('Arquivo enviado com sucesso!');
+        } else {
+          console.error('Erro ao enviar o arquivo:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Erro ao enviar o arquivo:', error);
+      }
+    });
+  
+    // Função para exibir uma mensagem na página
+    const showMessage = (message) => {
+      const successModal = document.getElementById('success-modal');
+      const modalContent = document.querySelector('#success-modal .modal-content');
+      const successMessage = document.createElement('p');
+      successMessage.textContent = message;
+      successMessage.classList.add('success-message');
+  
+      modalContent.innerHTML = ''; // Limpa o conteúdo anterior do modal
+      modalContent.appendChild(successMessage);
+      successModal.style.display = 'block';
+  
+      // Fecha o modal após alguns segundos (opcional)
+      setTimeout(() => {
+        successModal.style.display = 'none';
+      }, 3000); // Fecha o modal após 3 segundos (ajuste conforme necessário)
+    };
   
     // Atualize a lista de arquivos quando a página for carregada
     listFiles();
